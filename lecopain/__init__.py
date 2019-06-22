f5a5a753c2c2c46d9c54b23a4a0fd2f79c26a609
pigalon <pigalon@gmail.com>
2019-06-22 21:14:36 +0200

    remove db file and change db path

--- lecopain/__init__.py
+++ lecopain/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app                                   = Flask(__name__)
 app.config['SECRET_KEY']              = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/site.db'
 db                                    = SQLAlchemy(app)
 
 main                                  = Blueprint('main', __name__)
