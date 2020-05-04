0d3bafb00784bfda733996082280086ba3e70cd3
Pierrick MOLERA <pierrick@pop-os.localdomain>
2020-05-04 11:50:01 +0200

    # improve BaseTest

--- tests/base.py
+++ tests/base.py
@@ -1,16 +1,16 @@
-import unittest 
-import sys, os
+from lecopain.dao.models import User
+from lecopain import app, db
+import unittest
+import sys
+import os
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
 
-from lecopain import app, db
-from lecopain.dao.models import User
 
-
-class BaseTestCase(unittest.TestCase): 
+class BaseTestCase(unittest.TestCase):
     """A base test case."""
 
     def create_app(self):
@@ -18,11 +18,13 @@ class BaseTestCase(unittest.TestCase):
         return app
 
     def setUp(self):
-        app.config.from_object('config.TestConfig')
+        self.app = self.create_app()
         db.create_all()
-        db.session.add(User(username="admin", email="ad@min.com", password="admin"))
+        db.session.add(
+            User(username="admin", email="ad@min.com", password="admin"))
         db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        db.get_engine(self.app).dispose()
