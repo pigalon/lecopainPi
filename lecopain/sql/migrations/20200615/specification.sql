0371aaece9267fa8b93d21296a84e89cbf585b9a
pigalon <pigalon@gmail.com>
2020-06-15 16:33:38 +0200

    #26 prepare sql script

---
+++ lecopain/sql/migrations/20200615/specification.sql
@@ -0,0 +1,29 @@
+CREATE TABLE public.categories (
+    id integer DEFAULT nextval('public.category_id_seq'::regclass) NOT NULL,
+    name character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.categories OWNER TO postgres;
+
+CREATE TABLE public.specifications (
+    id integer DEFAULT nextval('public.category_id_seq'::regclass) NOT NULL,
+    name character varying(50) NOT NULL
+    subscription_id integer,
+);
+
+
+ALTER TABLE public.specifications OWNER TO postgres;
+
+ALTER TABLE ONLY public.specifications
+    ADD CONSTRAINT specifications_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id);
+
+
+ALTER TABLE public.products ADD type_id varchar NULL;
+ALTER TABLE public.products ADD category_id varchar NULL;
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id);
+
+ALTER TABLE public.lines ADD specification_id varchar NULL;
+ALTER TABLE public.subscription_lines ADD specification_id varchar NULL;
