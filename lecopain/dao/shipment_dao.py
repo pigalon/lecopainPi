1c607246cc70393f2c78af8c91a660e0b03e6101
pierrick <pierrick@pop-os.localdomain>
2020-07-13 20:24:37 +0200

    try fix odre in shipments

--- lecopain/dao/shipment_dao.py
+++ lecopain/dao/shipment_dao.py
@@ -117,6 +117,8 @@ class ShipmentDao:
         if customer_id != 0:
             all_shipments = all_shipments.filter(
                 Shipment.customer_id == customer_id)
+            
+        all_shipments = all_shipments.order_by(Shipment.id.desc())
 
         all_shipments = all_shipments.order_by(Shipment.shipping_dt.desc()) \
         .paginate(page=page, per_page=per_page)
